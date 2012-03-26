ba7c8db6bc436986d212aafd204000699369ec07
k1LoW <k1lowxb@gmail.com>
2012-03-26 17:50:29 +0900

    enabled setting

--- Controller/Component/YakComponent.php
+++ Controller/Component/YakComponent.php
@@ -165,17 +165,19 @@ class YakComponent extends Component {
     }
 
     public function beforeRender($controller) {
-        if ($this->emoji->isMobile()) {
-            $controller->response->type('xhtml');
+        if ($this->settings['enabled']) {
+            if ($this->emoji->isMobile()) {
+                $controller->response->type('xhtml');
 
-            if ($this->emoji->isSjisCarrier()) {
-                $controller->response->charset('Shift_JIS');
+                if ($this->emoji->isSjisCarrier()) {
+                    $controller->response->charset('Shift_JIS');
+                } else {
+                    $controller->response->charset('UTF-8');
+                }
             } else {
+                $controller->response->type('html');
                 $controller->response->charset('UTF-8');
             }
-        } else {
-            $controller->response->type('html');
-            $controller->response->charset('UTF-8');
         }
     }
 }
